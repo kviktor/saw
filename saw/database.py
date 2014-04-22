11c0fecdd6406a8e0e98c69fe1090d2b808c1383
kviktor <kviktor@cloud.bme.hu>
2014-04-22 20:32:28 +0200

    relative database path

--- saw/database.py
+++ saw/database.py
@@ -1,13 +1,18 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///saw.db', convert_unicode=True)
+path = os.getcwd()
+
+engine = create_engine("sqlite:///%s/saw.db" % path, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
+
 
 def init_db():
     # import all modules here that might define models so that
